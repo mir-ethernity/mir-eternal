7c36522d5affd11e9752c5d9a388f8d12789b23c
periko92 <109764547+periko92@users.noreply.github.com>
2022-07-22 20:15:57 +0200

    Update ViewData.cs
    translated

--- GameServer/GMCommands/ViewData.cs
+++ GameServer/GMCommands/ViewData.cs
@@ -20,10 +20,10 @@ namespace GameServer
 		// Token: 0x06000016 RID: 22 RVA: 0x00007EA4 File Offset: 0x000060A4
 		public override void 执行命令()
 		{
-			MainForm.添加命令日志("<= @" + base.GetType().Name + " 命令已经执行, 数据库详情如下:");
+			MainForm.添加命令日志("<= @" + base.GetType().Name + " The command has been executed and the database details are as follows:");
 			foreach (KeyValuePair<Type, DataTableBase> keyValuePair in GameDataGateway.Data型表)
 			{
-				MainForm.添加命令日志(string.Format("{0}  数量: {1}", keyValuePair.Value.DataType.Name, keyValuePair.Value.DataSheet.Count));
+				MainForm.添加命令日志(string.Format("{0}  Quantity: {1}", keyValuePair.Value.DataType.Name, keyValuePair.Value.DataSheet.Count));
 			}
 		}
 
