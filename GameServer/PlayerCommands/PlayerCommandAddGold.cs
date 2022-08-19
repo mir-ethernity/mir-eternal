316660f0956728a037d7a463c7bb65034d3e71a9
Bernat Vadell <bvadell@feimsoft.com>
2022-08-19 12:20:35 +0200

    feat(gs): add command gold

---
+++ GameServer/PlayerCommands/PlayerCommandAddGold.cs
@@ -0,0 +1,28 @@
+﻿using GameServer.Networking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer.PlayerCommands
+{
+
+    public class PlayerCommandAddGold : PlayerCommand
+    {
+        [Field(Position = 0)]
+        public int Amount;
+
+
+        public override void Execute()
+        {
+            Player.NumberGoldCoins += Amount;
+
+            Player.ActiveConnection?.SendPacket(new 货币数量变动
+            {
+                CurrencyType = 1,
+                货币数量 = Player.NumberGoldCoins
+            });
+        }
+    }
+}
