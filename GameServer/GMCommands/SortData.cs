6e5f45e68ff7e9e1f8e65f313157214811a4fb59
periko92 <erik.kill@hotmail.com>
2022-08-25 13:01:26 +0200

    (db) fixed mob spawns

--- GameServer/GMCommands/SortData.cs
+++ GameServer/GMCommands/SortData.cs
@@ -20,7 +20,7 @@ namespace GameServer
 		
 		public override void Execute()
 		{
-			if (MessageBox.Show("SortDataCommand needs to re-sort all customer data to save ID resources \r\n\r\nThis operation is irreversible, please make a data backup \r\n\r\n sure you want to do it?", "Dangerous operations", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation) == DialogResult.OK)
+			if (MessageBox.Show("SortDataCommand needs to re-sort all customer data to save ID resources \r\n\r\nThis operation is irreversible, please make a data  backup \r\n\r\n sure you want to do it?", "Dangerous operations", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation) == DialogResult.OK)
 			{
 				MainForm.AddCommandLog("<= @" + base.GetType().Name + " Start the command, do not close the window during the execution");
 				MainForm.Singleton.BeginInvoke(new MethodInvoker(delegate()
