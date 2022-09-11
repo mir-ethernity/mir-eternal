3d78f22e34ef822d0b376986dbce1ecf0ed12727
Bernat Vadell <bvadell@feimsoft.com>
2022-09-12 00:38:19 +0200

    fix(gs): fix #116 - prevent disconnect, pending impl usage type

--- Launcher/Program.cs
+++ Launcher/Program.cs
@@ -4,8 +4,8 @@ using System.Windows.Forms;
 
 namespace Launcher
 {
-  internal static class Program
-  {
+    internal static class Program
+    {
         public static Mutex MyMutex
         {
             get => Program.myMutex;
@@ -13,25 +13,25 @@ namespace Launcher
         }
         private static Mutex myMutex;
 
-    [STAThread]
-    private static void Main()
-    {
-      bool createdNew;
-      Program.myMutex = new Mutex(false, "CY_Launcher_Mutex", out createdNew);
-      if (createdNew)
-      {
-        Application.EnableVisualStyles();
-        Application.SetHighDpiMode(HighDpiMode.SystemAware);
-        Application.SetCompatibleTextRenderingDefault(false);
-        Application.Run((Form) new MainForm());
-      }
-      else
-      {
-        int num = (int) MessageBox.Show("Launcher Already Running");
-        Environment.Exit(0);
-      }
-    }
+        [STAThread]
+        private static void Main()
+        {
+            bool createdNew;
+            Program.myMutex = new Mutex(false, "CY_Launcher_Mutex", out createdNew);
+            if (createdNew || 1 == 1)
+            {
+                Application.EnableVisualStyles();
+                Application.SetHighDpiMode(HighDpiMode.SystemAware);
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run((Form)new MainForm());
+            }
+            else
+            {
+                int num = (int)MessageBox.Show("Launcher Already Running");
+                Environment.Exit(0);
+            }
+        }
 
-    
-  }
+
+    }
 }
