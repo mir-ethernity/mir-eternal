7e5ef406ca6e831b6ee787805adf25ae4dc6d23c
Bernat Vadell <bvadell@feimsoft.com>
2022-09-12 16:39:23 +0200

    feat(gs): add command "doquest" & "resetquests" to accelerate tests missions

---
+++ GameServer/PlayerCommands/PlayerCommandDoQuest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer.PlayerCommands
+{
+    public class PlayerCommandDoQuest : PlayerCommand
+    {
+        public override void Execute()
+        {
+            var quests = Player.CharacterData.GetInProgressQuests();
+
+            foreach (var quest in quests)
+            {
+                foreach (var mission in quest.Missions)
+                {
+                    if (mission.CompletedDate.V != DateTime.MinValue)
+                        continue;
+
+                    if (mission.Info.V.Type == Models.Enums.QuestMissionType.AdquireItem || mission.Info.V.Type == Models.Enums.QuestMissionType.KillMob)
+                        mission.Count.V = (byte)mission.Info.V.Count;
+
+                    mission.CompletedDate.V = MainProcess.CurrentTime;
+                }
+
+                Player.CompleteQuest(quest.Info.V.Id);
+            }
+        }
+    }
+}
