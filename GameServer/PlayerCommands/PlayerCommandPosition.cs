d8b5db4cd322b1bad658ba23b20403ccf62882af
Bernat Vadell <bvadell@feimsoft.com>
2022-09-23 08:40:04 +0200

    feat(gs): add command @position

---
+++ GameServer/PlayerCommands/PlayerCommandPosition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer.PlayerCommands
+{
+    public class PlayerCommandPosition : PlayerCommand
+    {
+        public override void Execute()
+        {
+            Player.SendMessage($"Your are in {Player.CurrentMap.MapId} ({Player.CurrentMap.地图模板.MapName}) at X: {Player.CurrentPosition.X}, Y: {Player.CurrentPosition.Y}");
+        }
+    }
+}
