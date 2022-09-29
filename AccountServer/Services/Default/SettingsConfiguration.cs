60205e9ee9a568f52645ca00b844d8c2409e76be
Bernat Vadell <bvadell@feimsoft.com>
2022-09-29 09:13:24 +0200

    feat(as): add service configuration

---
+++ AccountServer/Services/Default/SettingsConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using AccountServer.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountServer.Services.Default
+{
+    public class SettingsConfiguration : IAppConfiguration
+    {
+        public ushort LoginGatePort
+        {
+            get => Settings.Default.TSPort;
+            set
+            {
+                Settings.Default.TSPort = value;
+                Settings.Default.Save();
+            }
+        }
+        public ushort AccountServerPort
+        {
+            get => Settings.Default.ASPort;
+            set
+            {
+                Settings.Default.ASPort = value;
+                Settings.Default.Save();
+            }
+        }
+    }
+}
