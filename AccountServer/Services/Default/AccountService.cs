86f210937a786343708379843e4512feb961a414
Bernat Vadell <bvadell@feimsoft.com>
2022-09-29 09:13:36 +0200

    feat(as): add account service

---
+++ AccountServer/Services/Default/AccountService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountServer.Services.Default
+{
+    public class AccountService : IAccountService
+    {
+        private readonly Random Random = new();
+
+        public string RandomChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWQYZabcdefghijklmnopqrstuvwxyz";
+
+        public string GenerateTickets()
+        {
+            string text = "ULS21-";
+            for (int i = 0; i < 32; i++)
+            {
+                text += RandomChars[Random.Next(RandomChars.Length)].ToString();
+            }
+            return text;
+        }
+    }
+}
