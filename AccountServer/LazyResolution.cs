3fd420b093b2e598928d089f119115b9c9490bdb
Bernat Vadell <bvadell@feimsoft.com>
2022-09-29 09:17:44 +0200

    feat(as): add support to lazy resolution for circular deps

---
+++ AccountServer/LazyResolution.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountServer
+{
+    public static class LazyResolution
+    {
+        public static IServiceCollection AddLazyResolution(this IServiceCollection services)
+        {
+            return services.AddTransient(
+                typeof(Lazy<>),
+                typeof(LazilyResolved<>));
+        }
+
+        private class LazilyResolved<T> : Lazy<T>
+        {
+            public LazilyResolved(IServiceProvider serviceProvider)
+                : base(serviceProvider.GetRequiredService<T>)
+            {
+            }
+        }
+    }
+}
