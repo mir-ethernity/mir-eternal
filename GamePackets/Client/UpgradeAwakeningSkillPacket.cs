ad154073dd4f1f3a686238b929780c523ec0b0a0
Bernat Vadell <bvadell@feimsoft.com>
2022-09-29 19:35:13 +0200

    feat: add packet to upgrade awakening skill

---
+++ GamePackets/Client/UpgradeAwakeningSkillPacket.cs
@@ -0,0 +1,17 @@
+﻿using GameServer;
+using GameServer.Networking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePackets.Client
+{
+    [PacketInfo(Source = PacketSource.Client, Id = 226, Length = 4, Description = "UpgradeAwakeningSkillPacket")]
+    public class UpgradeAwakeningSkillPacket : GamePacket
+    {
+        [WrappingField(SubScript = 2, Length = 2)]
+        public ushort SkillId;
+    }
+}
