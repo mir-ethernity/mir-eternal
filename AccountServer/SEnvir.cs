6d098edf769a87f78c7c2fa735a425214be71b55
Bernat Vadell <bvadell@feimsoft.com>
2022-09-29 09:14:53 +0200

    feat: add empty senvir (we need move all logic to here)

---
+++ AccountServer/SEnvir.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountServer
+{
+    public class SEnvir
+    {
+        public async Task Start()
+        {
+
+        }
+
+        public async Task Stop()
+        {
+
+        }
+    }
+}
