7d864106d41a0a128b70dc511331cde1169e9070
Bernat Vadell <bvadell@feimsoft.com>
2022-09-29 09:14:13 +0200

    feat(as): add form richtextbox logger

---
+++ AccountServer/Services/Default/FormLogger.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountServer.Services.Default
+{
+    public class FormLogger : ILogger
+    {
+        private readonly MainForm _form;
+        private string _name;
+
+        public FormLogger(MainForm form, string name)
+        {
+            _form = form ?? throw new ArgumentNullException(nameof(form));
+            _name = name;
+        }
+
+        public IDisposable BeginScope<TState>(TState state) => default!;
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            if (!IsEnabled(logLevel)) return;
+
+            _form.LogInTextBox.AppendText($"[{logLevel}][{_name}] - {DateTime.Now:HH:mm:ss} - {formatter(state, exception)}{Environment.NewLine}");
+        }
+    }
+}
---
+++ AccountServer/Services/Default/FormLoggerProvider.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Services.Description;
+
+namespace AccountServer.Services.Default
+{
+    public class FormLoggerProvider : ILoggerProvider
+    {
+        private readonly ConcurrentDictionary<string, FormLogger> _loggers = new(StringComparer.OrdinalIgnoreCase);
+        private readonly IServiceProvider _services;
+
+        public FormLoggerProvider(IServiceProvider services)
+        {
+            _services = services ?? throw new ArgumentNullException(nameof(services));
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return _loggers.GetOrAdd(categoryName, name =>
+            {
+                return new FormLogger(_services.GetService<MainForm>(), name);
+            });
+        }
+
+        public void Dispose()
+        {
+            _loggers.Clear();
+        }
+    }
+}
