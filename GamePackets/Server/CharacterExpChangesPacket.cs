69c7402bfcc19fcea1a665c77401e80e8e2302ef
Bernat Vadell <bvadell@feimsoft.com>
2022-09-29 19:36:05 +0200

    feat: edit character exp change packet to include awakening exp

--- GamePackets/Server/CharacterExpChangesPacket.cs
+++ GamePackets/Server/CharacterExpChangesPacket.cs
@@ -2,38 +2,44 @@
 
 namespace GameServer.Networking
 {
-	
-	[PacketInfoAttribute(Source = PacketSource.Server, Id = 75, Length = 46, Description = "CharacterExpChangesPacket")]
-	public sealed class CharacterExpChangesPacket : GamePacket
-	{
-		
-		public CharacterExpChangesPacket()
-		{
-			
-			
-		}
 
-		
-		[WrappingFieldAttribute(SubScript = 2, Length = 4)]
-		public int 经验增加;
+    [PacketInfoAttribute(Source = PacketSource.Server, Id = 75, Length = 46, Description = "CharacterExpChangesPacket")]
+    public sealed class CharacterExpChangesPacket : GamePacket
+    {
 
-		
-		[WrappingFieldAttribute(SubScript = 6, Length = 4)]
-		public int 今日增加;
+        public CharacterExpChangesPacket()
+        {
 
-		
-		[WrappingFieldAttribute(SubScript = 10, Length = 4)]
-		public int 经验上限;
 
-		
-		[WrappingFieldAttribute(SubScript = 14, Length = 4)]
-		public int DoubleExp;
+        }
 
-		
-		[WrappingFieldAttribute(SubScript = 18, Length = 8)]
-		public long CurrentExp;
-		
-		[WrappingFieldAttribute(SubScript = 26, Length = 8)]
-		public long 升级所需;
-	}
+
+        [WrappingFieldAttribute(SubScript = 2, Length = 4)]
+        public int 经验增加;
+
+
+        [WrappingFieldAttribute(SubScript = 6, Length = 4)]
+        public int 今日增加;
+
+
+        [WrappingFieldAttribute(SubScript = 10, Length = 4)]
+        public int 经验上限;
+
+
+        [WrappingFieldAttribute(SubScript = 14, Length = 4)]
+        public int DoubleExp;
+
+
+        [WrappingFieldAttribute(SubScript = 18, Length = 8)]
+        public long CurrentExp;
+
+        [WrappingFieldAttribute(SubScript = 26, Length = 8)]
+        public long 升级所需;
+
+        [WrappingFieldAttribute(SubScript = 34, Length = 4)]
+        public int GainAwakeningExp;
+
+        [WrappingField(SubScript = 38, Length = 4)]
+        public int MaxAwakeningExp;
+    }
 }
