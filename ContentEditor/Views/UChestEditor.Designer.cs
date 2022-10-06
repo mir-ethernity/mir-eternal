d1e95efe8dfb3c0bdc805253fe8a3753fdb40ac3
Master <master@orzsoft.com>
2022-10-06 14:13:10 +0800

    Feat/db editor (#190)
    * fix translations zh-CN value
    
    * add translations more for zh-CN
    
    * fix translations more for zh-CN

---
+++ ContentEditor/Views/UChestEditor.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ContentEditor.Views
+{
+    partial class UChestEditor
+    {
+        private Sunny.UI.UIDataGridView DataGridChests;
+
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.DataGridChests = new Sunny.UI.UIDataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ChestName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridChests)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataGridChests
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            this.DataGridChests.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.DataGridChests.BackgroundColor = System.Drawing.Color.White;
+            this.DataGridChests.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DataGridChests.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.DataGridChests.ColumnHeadersHeight = 32;
+            this.DataGridChests.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.DataGridChests.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.ChestName});
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DataGridChests.DefaultCellStyle = dataGridViewCellStyle3;
+            this.DataGridChests.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DataGridChests.EnableHeadersVisualStyles = false;
+            this.DataGridChests.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DataGridChests.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
+            this.DataGridChests.Location = new System.Drawing.Point(0, 0);
+            this.DataGridChests.Margin = new System.Windows.Forms.Padding(5);
+            this.DataGridChests.Name = "DataGridChests";
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DataGridChests.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.DataGridChests.RowHeadersWidth = 62;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DataGridChests.RowsDefaultCellStyle = dataGridViewCellStyle5;
+            this.DataGridChests.RowTemplate.Height = 25;
+            this.DataGridChests.SelectedIndex = -1;
+            this.DataGridChests.Size = new System.Drawing.Size(1276, 893);
+            this.DataGridChests.StripeOddColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            this.DataGridChests.TabIndex = 0;
+            this.DataGridChests.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // Id
+            // 
+            this.Id.DataPropertyName = "Id";
+            this.Id.HeaderText = "Id";
+            this.Id.MinimumWidth = 8;
+            this.Id.Name = "Id";
+            this.Id.ReadOnly = true;
+            this.Id.Width = 150;
+            // 
+            // ChestName
+            // 
+            this.ChestName.DataPropertyName = "Name";
+            this.ChestName.HeaderText = "Name";
+            this.ChestName.MinimumWidth = 8;
+            this.ChestName.Name = "ChestName";
+            this.ChestName.Width = 150;
+            // 
+            // UChestEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.DataGridChests);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "UChestEditor";
+            this.Size = new System.Drawing.Size(1276, 893);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridChests)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DataGridViewTextBoxColumn Id;
+        private DataGridViewTextBoxColumn ChestName;
+    }
+}
---
+++ ContentEditor/Views/UChestEditor.cs
@@ -0,0 +1,81 @@
+﻿using ContentEditor.Models;
+using ContentEditor.Services;
+using GameServer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContentEditor.Views
+{
+    public partial class UChestEditor : UBaseEditor
+    {
+        public override string AttachedTabName => "TabChests";
+
+        public IDatabaseManager Database { get; private set; }
+
+        public UChestEditor()
+        {
+            InitializeComponent();
+            DataGridChests.AutoGenerateColumns = false;
+            DataGridChests.DataError += DataGridChests_DataError;
+            DataGridChests.CellDoubleClick += DataGridChests_CellDoubleClick;
+        }
+
+        private void DataGridChests_CellDoubleClick(object? sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+                return;
+
+            var row = DataGridChests.Rows[e.RowIndex];
+
+            if (row.IsNewRow)
+                return;
+
+            var map = (GameItem)row.DataBoundItem;
+        }
+
+        private void DataGridChests_DataError(object? sender, DataGridViewDataErrorEventArgs e)
+        {
+
+        }
+
+        public override void ReloadDatabase(IDatabaseManager database)
+        {
+            Database = database;
+
+            DataGridChests.DataSource = database.Item.DataSource;
+            DataGridChests.Refresh();
+        }
+
+        private void ConfigureDropDownForEnum<TEnum>(DataGridViewComboBoxColumn dropdown) where TEnum : struct
+        {
+            var options = GetOptionsFromEnum<TEnum>();
+
+            dropdown.DataSource = options;
+            dropdown.ValueMember = "Key";
+            dropdown.DisplayMember = "Value";
+        }
+
+        private List<KeyValuePair<string, string>> GetOptionsFromEnum<TEnum>() where TEnum : struct
+        {
+            if (!typeof(TEnum).IsEnum) throw new ArgumentException("TEnum must be an enumerated type");
+
+            var options = new List<KeyValuePair<string, string>>();
+
+            var values = Enum.GetValues(typeof(TEnum));
+
+            foreach (var value in values)
+            {
+                options.Add(new KeyValuePair<string, string>(value.ToString() ?? "-1", Enum.GetName(typeof(TEnum), value) ?? "Unknown"));
+            }
+
+            return options;
+        }
+    }
+}
